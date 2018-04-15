d19fdd15ffbe1dea226bf44ca969c505de551148
Derrick Mullins <deemullins22@gmail.com>
2018-04-15 16:05:15 -0500

    Initial commit

---
+++ src/com/company/LList.java
@@ -0,0 +1,49 @@
+package com.company;
+
+public class LList {
+    LList next;
+    int value;
+
+    public LList() {
+        next = null;
+        value = 0;
+    }
+    public LList(int value) {
+        this.value = value;
+        next = null;
+    }
+    public LList(int num, LList next) {
+        value = num;
+        this.next = next;
+    }
+
+    public LList(LList rhs) {
+        value = rhs.value;
+        this.next = rhs;
+    }
+
+    public LList getNext() {
+        return next;
+    }
+
+    public void setNext(LList next) {
+        this.next = next;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "LList{" +
+                "next=" + next +
+                ", value=" + value +
+                '}';
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! ");
+
+        LList n1 = new LList();
+        LList n2 = new LList(10, n1.next);
+        LList n3 = new LList(20);
+        System.out.println(n1);
+
+    }
+}
